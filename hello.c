5e0e4dced215447d1276454c4f970bae3873eb90
Daisy <vtvanathi72@gmail.com>
2026-06-20 20:55:25 +0530

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,11 +1,2 @@
 C Programming
-
-This repository contains my C programming practice programs while preparing for GATE CSE 2027.
-
-Topics:
-- Basics
-- Variables
-- Operators
-- Loops
-- Functions
-- Arrays
+ 
